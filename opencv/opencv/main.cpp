c182cc414334e5a932dacebf7f2f41e0c57b49b9
lilly9928 <50159938+lilly9928@users.noreply.github.com>
2021-04-15 16:09:19 +0900

    공부

--- opencv/opencv/main.cpp
+++ opencv/opencv/main.cpp
@@ -4,35 +4,25 @@
 //
 //  Created by kawaii on 2021/04/07.
 //
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
-#include "opencv2/opencv.hpp"
 using namespace cv;
+using namespace std;
 
-Mat src, src_gray;
-Mat dst, detected_edges;
-int edgeThresh = 1;
-int lowThreshold;
-int const max_lowThreshold = 100;
-int ratio = 3;
-int kernel_size = 3;
+int main() {
+    Mat src = imread("Lenna.jpg", IMREAD_COLOR);
+    if (src.empty()) return 1;
 
-static void CannyThreshold(int, void*)
-{
-    blur(src, detected_edges, Size(3, 3));
-    Canny(detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size);
-    dst = Scalar::all(0);
-    src.copyTo(dst, detected_edges);
-    imshow("Image", src);
-    imshow("Canny", dst);
-}
-int main()
-{
-    src = imread("Lenna.jpg", IMREAD_GRAYSCALE);
-    if (src.empty()) { return -1; }
-    dst.create(src.size(), src.type());
-    namedWindow("Canny");
-    createTrackbar("Min Threshold:", "Canny", &lowThreshold, max_lowThreshold, CannyThreshold);
-    CannyThreshold(0, 0);
+    Mat dst;
+    float weights[9] = { -1, -1, -1, -1, 9, -1, -1, -1, -1 };
+
+    Mat mask = Mat(3, 3, CV_32F, weights);
+
+    filter2D(src, dst, -1, mask, Point(-1, -1), 0, BORDER_DEFAULT);
+    imshow("src", src);
+    imshow("sharpen", dst);
+
     waitKey(0);
     return 0;
 }
