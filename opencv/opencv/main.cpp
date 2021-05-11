e5b6eb5c2abc923c267e0c0e6a494a1100119bbb
lilly9928 <50159938+lilly9928@users.noreply.github.com>
2021-05-11 16:29:09 +0900

    test

--- opencv/opencv/main.cpp
+++ opencv/opencv/main.cpp
@@ -9,79 +9,15 @@
 using namespace cv;
 using namespace std;
 
-void displayDFT(Mat& src){
-    Mat image_array[2] = {Mat::zeros(src.size(),CV_32F), Mat::zeros(src.size(),CV_32F)};
-    split(src, image_array);
-    
-    Mat mag_image;
-    magnitude(image_array[0], image_array[1], mag_image);
-    mag_image += Scalar::all(1);
-    log(mag_image,mag_image);
-    
-    normalize(mag_image,mag_image,0,1,NORM_MINMAX);
-    imshow("DFT", mag_image);
-   // waitKey(0);
-}
-
-void shuffleDFT(Mat& src){
-    int cX = src.cols/2;
-    int cY = src.rows/2;
-    
-    Mat q1(src,Rect(0,0,cX,cY));
-    Mat q2(src,Rect(cX,0,cX,cY));
-    Mat q3(src,Rect(0,cY,cX,cY));
-    Mat q4(src,Rect(cX,cY,cX,cY));
-    
-    Mat tmp;
-    q1.copyTo(tmp);
-    q4.copyTo(q1);
-    tmp.copyTo(q4);
-    q2.copyTo(tmp);
-    q3.copyTo(q2);
-    tmp.copyTo(q3);
-           
-}
-
-Mat getFilter(Size size){
-    Mat tmp = Mat(size,CV_32F);
-    for(int i = 0; i < tmp.rows ; i++){
-        for(int j = 0; j <tmp.cols; j++){
-            if(i>(tmp.rows/2 - 10)&& i < (tmp.rows/2 +10)&&j > (tmp.cols/2 +10))
-                tmp.at<float>(i,j) = 0;
-            else if (i > (tmp.rows/2 - 10)&& i < (tmp.rows/2 + 10)&& j < (tmp.cols/2 - 10))
-                tmp.at<float>(i,j) = 0;
-            else tmp.at<float>(i,j)= 1;
-        }
-    }
-    Mat toMerge[] = {tmp,tmp};
-    Mat filter;
-    merge(toMerge, 2, filter);
-    return filter;
-}
-
-
 int main()
 {
-    Mat src =imread("test.jpeg",IMREAD_GRAYSCALE);
-    Mat src_float,dft_images;
-    imshow("original", src);
-    
-    src.convertTo(src_float, CV_32FC1,1.0/255.0);
-    dft(src_float,dft_images,DFT_COMPLEX_OUTPUT);
-    shuffleDFT(dft_images);
-    displayDFT(dft_images);
-    
-    Mat lowpass = getFilter(dft_images.size());
-    Mat result;
+    Mat src =imread("sample2.jpeg",IMREAD_GRAYSCALE);
 
-    multiply(dft_images, lowpass, result);
-    displayDFT(result);
-
-    Mat inverted_images;
-    shuffleDFT(result);
-    idft(result, inverted_images, DFT_SCALE| DFT_REAL_OUTPUT);
-    imshow("test", inverted_images);
+    Mat dst;
+    equalizeHist(src, dst);
+    imshow("src", src);
+    imshow("dst", dst);
     waitKey(0);
-    return 1;
+    return 0;
 
 }
